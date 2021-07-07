62a501e4826509c716d2b2fe83f5e8dfebf4d993
ruthschoebel <ruth.schoebel@rwth-aachen.de>
2021-07-07 21:20:08 +0200

    new branch

---
+++ pySDC/implementations/problem_classes/Heat_MPIFFT.py
@@ -0,0 +1,293 @@
+import numpy as np
+from mpi4py import MPI
+from mpi4py_fft import PFFT
+
+from pySDC.core.Errors import ParameterError, ProblemError
+from pySDC.core.Problem import ptype
+from pySDC.implementations.datatype_classes.mesh import mesh#, imex_mesh
+
+from mpi4py_fft import newDistArray
+
+
+class heat(ptype):
+    """
+    Example implementing the nonlinear Schrödinger equation in 2-3D using mpi4py-fft for solving linear parts,
+    IMEX time-stepping
+
+    mpi4py-fft: https://mpi4py-fft.readthedocs.io/en/latest/
+
+    Attributes:
+        fft: fft object
+        X: grid coordinates in real space
+        K2: Laplace operator in spectral space
+    """
+
+    def __init__(self, problem_params, dtype_u=mesh, dtype_f=mesh):
+        """
+        Initialization routine
+
+        Args:
+            problem_params (dict): custom parameters for the example
+            dtype_u: fft data type (will be passed to parent class)
+            dtype_f: fft data type wuth implicit and explicit parts (will be passed to parent class)
+        """
+        if False:
+            if 'L' not in problem_params:
+                problem_params['L'] = 1.0 #2.0 * np.pi
+            # if 'init_type' not in problem_params:
+            #     problem_params['init_type'] = 'circle'
+            if 'comm' not in problem_params:
+                problem_params['comm'] = MPI.COMM_WORLD
+            if 'c' not in problem_params:
+                problem_params['c'] = 1.0
+
+            #if not problem_params['L'] == 2.0 * np.pi:
+            #    raise ProblemError(f'Setup not implemented, L has to be 2pi, got {problem_params["L"]}')
+
+            if not (problem_params['c'] == 0.0 or problem_params['c'] == 1.0):
+                raise ProblemError(f'Setup not implemented, c has to be 0 or 1, got {problem_params["c"]}')
+
+            # these parameters will be used later, so assert their existence
+            essential_keys = ['nvars', 'c', 'L', 'spectral']
+            for key in essential_keys:
+                if key not in problem_params:
+                    msg = 'need %s to instantiate problem, only got %s' % (key, str(problem_params.keys()))
+                    raise ParameterError(msg)
+
+            if not (isinstance(problem_params['nvars'], tuple) and len(problem_params['nvars']) > 1):
+                raise ProblemError('Need at least two dimensions')
+
+            # Creating FFT structure
+            self.ndim = len(problem_params['nvars'])
+            axes = tuple(range(self.ndim))
+            self.fft = PFFT(problem_params['comm'], list(problem_params['nvars']), axes=axes, dtype=np.float, #complex128,
+                            collapse=True)
+
+            # get test data to figure out type and dimensions
+            tmp_u = newDistArray(self.fft, problem_params['spectral'])
+
+            # invoke super init, passing the communicator and the local dimensions as init
+            super(heat, self).__init__(init=(tmp_u.shape, problem_params['comm'], tmp_u.dtype),
+                                                             dtype_u=dtype_u, dtype_f=dtype_f, params=problem_params)
+
+            self.L = np.array([self.params.L] * self.ndim, dtype=np.float)#complex128)
+
+            # get local mesh
+            X = np.ogrid[self.fft.local_slice(False)]
+            N = self.fft.global_shape()
+            for i in range(len(N)):
+                X[i] = (X[i] * self.L[i] / N[i])
+            self.X = [np.broadcast_to(x, self.fft.shape(False)) for x in X]
+
+            # get local wavenumbers and Laplace operator
+            s = self.fft.local_slice()
+            N = self.fft.global_shape()
+            k = [np.fft.fftfreq(n, 1. / n).astype(int) for n in N]
+            K = [ki[si] for ki, si in zip(k, s)]
+            Ks = np.meshgrid(*K, indexing='ij', sparse=True)
+            Lp = 2 * np.pi / self.L
+            for i in range(self.ndim):
+                Ks[i] = (Ks[i] * Lp[i]).astype(np.complex128)
+            K = [np.broadcast_to(k, self.fft.shape(True)) for k in Ks]
+            K = np.array(K).astype(np.complex128)
+            self.K2 = np.sum(K * K, 0, dtype=np.complex128)
+            #print("K2", self.K2.shape)
+
+
+
+
+        if True:
+
+            if 'L' not in problem_params:
+                problem_params['L'] = 1.0
+            if 'comm' not in problem_params:
+                problem_params['comm'] = None
+
+            # these parameters will be used later, so assert their existence
+            essential_keys = ['nvars', 'spectral']
+
+            for key in essential_keys:
+                if key not in problem_params:
+                    msg = 'need %s to instantiate problem, only got %s' % (key, str(problem_params.keys()))
+                    raise ParameterError(msg)
+
+            if not (isinstance(problem_params['nvars'], tuple) and len(problem_params['nvars']) > 1):
+                raise ProblemError('Need at least two dimensions')
+
+            # Creating FFT structure
+            ndim = len(problem_params['nvars'])
+            axes = tuple(range(ndim))
+            self.fft = PFFT(problem_params['comm'], list(problem_params['nvars']), axes=axes, dtype=np.float, collapse=True)
+
+            # get test data to figure out type and dimensions
+            tmp_u = newDistArray(self.fft, problem_params['spectral'])
+
+            # invoke super init, passing the communicator and the local dimensions as init
+            super(heat, self).__init__(init=(tmp_u.shape, problem_params['comm'], tmp_u.dtype),
+                                                 dtype_u=dtype_u, dtype_f=dtype_f, params=problem_params)
+
+            L = np.array([self.params.L] * ndim, dtype=float)
+
+            # get local mesh
+            X = np.ogrid[self.fft.local_slice(False)]
+            N = self.fft.global_shape()
+            for i in range(len(N)):
+                X[i] = (X[i] * L[i] / N[i])
+            #print("X", X)
+            self.X = X #[np.broadcast_to(x, self.fft.shape(False)) for x in X]
+
+            # get local wavenumbers and Laplace operator
+            s = self.fft.local_slice()
+            N = self.fft.global_shape()
+            k = [np.fft.fftfreq(n, 1. / n).astype(int) for n in N[:-1]]
+            k.append(np.fft.rfftfreq(N[-1], 1. / N[-1]).astype(int))
+            K = [ki[si] for ki, si in zip(k, s)]
+            Ks = np.meshgrid(*K, indexing='ij', sparse=True)
+            Lp = 2 * np.pi / L
+            for i in range(ndim):
+                Ks[i] = (Ks[i] * Lp[i]).astype(float)
+            K = [np.broadcast_to(k, self.fft.shape(True)) for k in Ks]
+            K = np.array(K).astype(float)
+            self.K2 = np.sum(K * K, 0, dtype=float)
+
+
+        # Need this for diagnostics
+        self.dx = self.params.L / problem_params['nvars'][0]
+        self.dy = self.params.L / problem_params['nvars'][1]
+
+
+
+        self.model = problem_params['model']
+        self.model_params = problem_params['model_params'] 
+        self.subkey =  problem_params['subkey'] 
+        self.rng_key = problem_params['rng_key'] 
+        self.time_comm = problem_params['time_comm'] 
+        self.time_rank = self.time_comm.Get_rank()
+        self.space_comm = problem_params['comm']
+        self.space_rank = self.space_comm.Get_rank()
+        self.iters = 0
+        self.size = 0
+
+        print(self.time_rank , self.space_rank)
+
+        if problem_params['use_RL']:
+            #self.QD = self.dtype_u(self.init)
+            self.QD = np.ndarray(shape=self.K2.shape, dtype=float) #complex)    #newDistArray(self.fft, True) 
+            #for idx, x in np.ndenumerate(self.K2):
+            #    self.QD[idx] = self.model(self.model_params, -x*1j)[0][self.time_rank] #, rng=self.subkey
+
+            tmp = np.ndarray(shape=(self.K2.shape[0]*self.K2.shape[1],1),dtype=float, buffer= (-self.K2).flatten() )#np.array(self.K2.flatten(), dtype=complex)) #self.K2.flatten())
+            #tmp *= -1j
+            #tmp2 = self.model(self.model_params, tmp)[:,self.time_rank].reshape(self.K2.shape[0], self.K2.shape[1])
+            #print(self.time_rank, self.model(self.model_params, tmp)[:,self.time_rank])
+            #print((tmp2.reshape(self.K2.shape[0], self.K2.shape[1])).shape, self.QD.shape)
+
+            #for idx, x in np.ndenumerate(self.K2):
+            #    #self.QD[idx] = tmp2[idx]
+            #    if abs(self.QD[idx] - tmp2[idx])>0.0001:
+            #        print(idx, self.QD[idx], tmp2[idx])
+            self.QD[:,:] = self.model(self.model_params, tmp)[:,self.time_rank].reshape(self.K2.shape[0], self.K2.shape[1])    #tmp2#.reshape(self.K2.shape[0], self.K2.shape[1])[:]
+    #def createQI(self,k):
+    #    if self.ist:
+    #        self.QD2 = np.ndarray(shape=self.K2.shape) 
+    #        for idx, x in np.ndenumerate(self.K2):
+    #            self.QD2[idx] = k
+
+    #        print("max", self.QD2.shape, self.QD.shape, np.max(self.QD2-self.QD))
+    #    self.ist=False
+
+    def multQI(self, x):
+        f = self.dtype_u(self.init)
+
+        f = x*self.QD 
+        return f
+
+
+    def eval_f(self, u, t):
+        """
+        Routine to evaluate the RHS
+
+        Args:  
+            u (dtype_u): current values
+            t (float): current time
+
+        Returns:
+            dtype_f: the RHS
+        """
+
+        f = self.dtype_f(self.init)
+
+        if self.params.spectral:
+            f = -self.K2 * u
+
+        else:
+
+            u_hat = self.fft.forward(u)
+            lap_u_hat = -self.K2 * u_hat
+            f[:] = self.fft.backward(lap_u_hat, f)
+
+        return f
+
+    def solve_system(self, rhs, factor, u0, t):
+        """
+        Simple FFT solver for the diffusion part
+
+        Args:
+            rhs (dtype_f): right-hand side for the linear system
+            factor (float) : abbrev. for the node-to-node stepsize (or any other factor required)
+            u0 (dtype_u): initial guess for the iterative solver (not used here so far)
+            t (float): current time (e.g. for time-dependent BCs)
+
+        Returns:
+            dtype_u: solution as mesh
+        """
+
+        self.iters+=1
+
+        self.size = rhs.size
+
+        if self.params.spectral:
+
+            me = rhs / (1.0 + factor * self.K2)
+
+        else:
+
+            me = self.dtype_u(self.init)
+            rhs_hat = self.fft.forward(rhs)
+            rhs_hat /= (1.0 + factor * self.K2)
+            me[:] = self.fft.backward(rhs_hat)
+
+        return me
+
+    def u_exact(self, t):
+        """
+        Routine to compute the exact solution at time t, see (1.3) https://arxiv.org/pdf/nlin/0702010.pdf for details
+
+        Args:
+            t (float): current time
+
+        Returns:
+            dtype_u: exact solution
+        """
+
+
+
+
+        tmp_me = self.dtype_u(self.init, val=1.0) 
+        u = self.dtype_u(self.init, val=1.0) 
+
+
+        if self.params.spectral:
+            tmp= np.sin(2*np.pi * self.X[0])*np.sin(2*np.pi * self.X[1]) * np.exp(-t * 2 *(2* np.pi)**2)
+            
+            tmp_me[:] = self.fft.forward(tmp)
+
+
+        else:
+
+            tmp_me[:] = np.sin(2*np.pi * self.X[0])*np.sin(2*np.pi * self.X[1]) * np.exp(-t * 2 *(2* np.pi)**2)
+
+
+
+        return tmp_me
+        #return me
---
+++ pySDC/tutorial/step_7/schroedinger_pySDC_with_mpi4pyfft.py
@@ -0,0 +1,300 @@
+from argparse import ArgumentParser
+import numpy as np
+from mpi4py import MPI
+
+from pySDC.helpers.stats_helper import filter_stats, sort_stats
+from pySDC.implementations.collocation_classes.gauss_radau_right import CollGaussRadau_Right
+from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
+from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
+from pySDC.projects.parallelSDC.generic_imex_MPI import generic_imex_MPI
+from pySDC.implementations.problem_classes.NonlinearSchroedinger_MPIFFT import nonlinearschroedinger_imex
+#from pySDC.implementations.transfer_classes.TransferMesh_MPIFFT import fft_to_fft
+
+
+import jax
+import jax.numpy as jnp
+from jax.experimental import stax
+from jax.experimental import optimizers
+
+def build_model(M):
+    scale = 1e-3
+    glorot_normal = jax.nn.initializers.variance_scaling(
+        scale, "fan_avg", "truncated_normal")
+    normal = jax.nn.initializers.normal(scale)
+    (model_init, model_apply) = stax.serial(
+        stax.Dense(64, glorot_normal, normal),
+        stax.Relu,
+        # stax.Dense(256),
+        # stax.Relu,
+        stax.Dense(64, glorot_normal, normal),
+        stax.Relu,
+        stax.Dense(M, glorot_normal, normal),
+    )
+    return (model_init, model_apply)
+
+
+
+
+def load_model(path):
+    with open(path, 'rb') as f:
+        weights = jnp.load(f, allow_pickle=True)
+    with open(str(path) + '.steps', 'rb') as f:
+        steps = jnp.load(f, allow_pickle=True)
+    return weights, steps
+
+def run_simulation(spectral=None, ml=None, nprocs_space=None, sweeper_class=None, use_RL = None):
+    """
+    A test program to do SDC, MLSDC and PFASST runs for the 2D NLS equation
+
+    Args:
+        spectral (bool): run in real or spectral space
+        ml (bool): single or multiple levels
+        num_procs (int): number of parallel processors
+    """
+
+    num_nodes = 3
+    seed = 0
+    eval_seed = seed
+    if eval_seed is not None:
+        eval_seed += 1
+
+    rng_key = jax.random.PRNGKey(0)
+    model_path = "models/complex_model_2021-06-29T12-51-32.544928.npy"
+
+    model_init, model = build_model(num_nodes)
+    rng_key, subkey = jax.random.split(rng_key)
+   
+    params, _ = load_model(model_path)
+
+    #rng_key = jax.random.PRNGKey(0)
+    #rng_key, subkey = jax.random.split(rng_key)
+
+
+
+    #rng_key, subkey = jax.random.split(rng_key)
+
+    #model_path = "complex_model_2021-06-29T12-51-32.544928.npy" #"dp_model_2021-06-24T09-55-45.128649.npy"
+
+    #params, _ = load_model(model_path)
+
+
+    #_, model = build_model(3, train=False)
+
+
+
+    #action = model(params, -1j, rng=subkey)
+
+
+    #model = jax.jit(model) 
+
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+
+    world_rank = comm.Get_rank()
+    world_size = comm.Get_size()
+
+
+    assert world_size%nprocs_space == 0, 'Total number of processors must be multiple of number of processors in space'  
+
+    # split world communicator to create space-communicators
+    if nprocs_space is not None:
+        color = int(world_rank / nprocs_space)
+    else:
+        color = int(world_rank / 1)
+    space_comm = comm.Split(color=color)
+    space_comm.Set_name('Space-Comm')
+    space_size = space_comm.Get_size()
+    space_rank = space_comm.Get_rank()
+
+    # split world communicator to create time-communicators
+    if nprocs_space is not None:
+        color = int(world_rank % nprocs_space)
+    else:
+        color = int(world_rank / world_size)
+    time_comm = comm.Split(color=color)
+    time_comm.Set_name('Time-Comm')
+    time_size = time_comm.Get_size()
+    time_rank = time_comm.Get_rank()
+
+
+
+
+
+    # initialize level parameters
+    level_params = dict()
+    level_params['restol'] = 1E-08
+    level_params['dt'] = 1E-01 / 2
+    level_params['nsweeps'] = [1]
+
+    # initialize sweeper parameters
+    sweeper_params = dict()
+    sweeper_params['collocation_class'] = CollGaussRadau_Right
+    sweeper_params['num_nodes'] = [num_nodes]
+
+    if sweeper_class == 'generic_imex_MPI':
+        assert sweeper_params['num_nodes'][0] == time_size, 'Need %s processors in time' % sweeper_params['num_nodes']   
+
+    sweeper_params['initial_guess'] = 'zero'
+
+
+    sweeper_params['comm'] = time_comm #MPI.COMM_WORLD
+
+    # initialize problem parameters
+    problem_params = dict()
+    if ml:
+        problem_params['nvars'] = [(128, 128), (32, 32)]
+    else:
+        problem_params['nvars'] = [(32, 32)]
+    problem_params['spectral'] = spectral
+    problem_params['comm'] = space_comm
+    problem_params['time_comm'] = time_comm
+    problem_params['model'] = model
+    problem_params['model_params'] = params
+    problem_params['subkey'] = subkey
+    problem_params['rng_key'] = rng_key
+
+
+    # initialize step parameters
+    step_params = dict()
+    step_params['maxiter'] = 100
+
+    # initialize controller parameters
+    controller_params = dict()
+    controller_params['logger_level'] = 30 if rank == 0 else 99
+    # controller_params['predict_type'] = 'fine_only'
+
+    # fill description dictionary for easy step instantiation
+    description = dict()
+    description['problem_params'] = problem_params  # pass problem parameters
+    description['problem_class'] = nonlinearschroedinger_imex
+    description['sweeper_class'] = sweeper_class #generic_imex_MPI # imex_1st_order
+    if sweeper_class == generic_imex_MPI:
+        if use_RL:
+            sweeper_params['QI'] = ['RL'] 
+            problem_params['use_RL'] = True 
+
+        else:
+            sweeper_params['QI'] = ['MIN'] 
+            problem_params['use_RL'] = False 
+
+    else:
+        sweeper_params['QI'] = ['LU']  
+        problem_params['use_RL'] = False 
+
+
+
+
+
+    description['sweeper_params'] = sweeper_params  # pass sweeper parameters
+    description['level_params'] = level_params  # pass level parameters
+    description['step_params'] = step_params  # pass step parameters
+    #description['space_transfer_class'] = fft_to_fft
+
+
+
+
+    # set time parameters
+    t0 = 0.0
+    Tend = 1.0
+
+    f = None
+    if rank == 0:
+        f = open('step_7_B_out.txt', 'a')
+        out = f'Running with ml={ml} and num_procs={world_size}...'
+        f.write(out + '\n')
+        print(out)
+
+    # instantiate controller
+    controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+
+    # get initial values on finest level
+    P = controller.MS[0].levels[0].prob
+    uinit = P.u_exact(t0)
+
+    # call main function to get things done...
+    uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
+    uex = P.u_exact(Tend)
+    err = abs(uex - uend)
+
+    print(world_rank, err, P.iters, P.size)
+
+    #print(world_rank, niter )
+
+    if rank == 0:
+        # filter statistics by type (number of iterations)
+        filtered_stats = filter_stats(stats, type='niter')
+
+        # convert filtered statistics to list of iterations count, sorted by process
+        iter_counts = sort_stats(filtered_stats, sortby='time')
+
+        niters = np.array([item[1] for item in iter_counts])
+        out = f'   Min/Mean/Max number of iterations: ' \
+              f'{np.min(niters):4.2f} / {np.mean(niters):4.2f} / {np.max(niters):4.2f}'
+        f.write(out + '\n')
+        print(out)
+        out = '   Range of values for number of iterations: %2i ' % np.ptp(niters)
+        f.write(out + '\n')
+        print(out)
+        out = '   Position of max/min number of iterations: %2i -- %2i' % \
+              (int(np.argmax(niters)), int(np.argmin(niters)))
+        f.write(out + '\n')
+        print(out)
+        out = '   Std and var for number of iterations: %4.2f -- %4.2f' % (float(np.std(niters)), float(np.var(niters)))
+        f.write(out + '\n')
+        print(out)
+
+        out = f'Error: {err:6.4e}'
+        f.write(out + '\n')
+        print(out)
+
+        timing = sort_stats(filter_stats(stats, type='timing_run'), sortby='time')
+        out = f'Time to solution: {timing[0][1]:6.4f} sec.'
+        f.write(out + '\n')
+        print(out)
+
+        assert err <= 1.133E-05, 'Error is too high, got %s' % err
+        if ml:
+            if num_procs > 1:
+                maxmean = 12.5
+            else:
+                maxmean = 6.6
+        else:
+            maxmean = 12.7
+        #assert np.mean(niters) <= maxmean, 'Mean number of iterations is too high, got %s' % np.mean(niters)
+
+        print('Mean number of iterations %s' %( np.mean(niters)))
+        if np.mean(niters) > maxmean:
+            print('Mean number of iterations is too high, got %s expected %s' %( np.mean(niters), maxmean))
+
+        f.write('\n')
+        print()
+        f.close()
+
+
+def main():
+    """
+    Little helper routine to run the whole thing
+
+    Note: This can also be run with "mpirun -np 2 python B_pySDC_with_mpi4pyfft.py"
+    """
+    parser = ArgumentParser()
+    parser.add_argument("-n", "--nprocs_space", help='Specifies the number of processors in space', type=int)
+    args = parser.parse_args()
+
+
+
+    #print("############ RL")
+    run_simulation(spectral=True, ml=False, nprocs_space=args.nprocs_space, sweeper_class = generic_imex_MPI, use_RL = True)
+    #print("############ MIN")
+    run_simulation(spectral=True, ml=False, nprocs_space=args.nprocs_space, sweeper_class = generic_imex_MPI, use_RL = False)
+    #print("############ LU")
+    run_simulation(spectral=True, ml=False, nprocs_space=6, sweeper_class = imex_1st_order, use_RL = False)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
