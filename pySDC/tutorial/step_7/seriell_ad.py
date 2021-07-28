4ad71abf77a73f507e8a1dc33461d6da1cb8859e
ruthschoebel <ruth.schoebel@rwth-aachen.de>
2021-07-28 14:47:04 +0200

    jureca

--- pySDC/tutorial/step_7/seriell_ad.py
+++ pySDC/tutorial/step_7/seriell_ad.py
@@ -428,7 +428,7 @@ def main():
 
     n_space = int(world_size/num_nodes)
 
-    print("aufteilung", world_size, n_space)
+    #print("aufteilung", world_size, n_space)
 
     #MPI.COMM_WORLD.Barrier()    
     #if rank ==0: print("############ RL+0")
@@ -455,7 +455,7 @@ def main():
     MPI.COMM_WORLD.Barrier()    
     run_simulation(spectral=True, ml=False, nprocs_space=world_size, sweeper_class = generic_implicit, use_RL = False, MIN3=False, RL_both=False, index=4, imex=False)
     MPI.COMM_WORLD.Barrier()   
-    if rank ==0: plot()
+    #if rank ==0: plot()
     #run_simulation(spectral=True, ml=False, nprocs_space=1, sweeper_class = imex_1st_order, use_RL = False)
 
 
